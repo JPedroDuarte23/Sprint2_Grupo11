b209d9db33ec268b29ab5bed006e1e9816149ca3
Rian <127910242+Rian-Ferrari@users.noreply.github.com>
2023-03-28 15:01:04 -0300

    Update BD_sprint2.sql
    Nicolas Nunes dos Santos

--- Banco de Dados/BD_sprint2.sql
+++ Banco de Dados/BD_sprint2.sql
@@ -1,34 +1,32 @@
 CREATE DATABASE sprint2;
 USE sprint2;
 
-CREATE TABLE CADASTRO (
-    UserID INT PRIMARY KEY AUTO_INCREMENT,
-    Email VARCHAR(50),
-    Senha VARCHAR(40),
-    NomeFantasia VARCHAR(40)
+CREATE TABLE EMPRESA (
+    IDEmpresa INT PRIMARY KEY AUTO_INCREMENT,
+    Email VARCHAR(50) NOT NULL,
+    Senha VARCHAR(40) NOT NULL,
+    Empresa VARCHAR(50) NOT NULL,
 );
 CREATE TABLE LOTE (
     IDLote INT PRIMARY KEY AUTO_INCREMENT,
-    Lote VARCHAR(50),
+    Serial VARCHAR(50) NOT NULL,
     fk_Lote_Empresa INT,
     fk_Lote_Sensor INT,
-    CONSTRAINT fk_Lote_Empresa FOREIGN KEY (fk_Lote_Empresa) REFERENCES CADASTRO(UserID),
+    CONSTRAINT fk_Lote_Empresa FOREIGN KEY (fk_Lote_Empresa) REFERENCES EMPRESA(IDEmpresa),
     CONSTRAINT fk_Lote_Sensor FOREIGN KEY (fk_Lote_Sensor) REFERENCES SENSOR(SensorID)
 );
 CREATE TABLE SENSOR (
-    SensorID INT PRIMARY KEY AUTO_INCREMENT,
-    Nome VARCHAR(10),
+    IDSensor INT PRIMARY KEY AUTO_INCREMENT,
     fk_Empresa INT,
     Instalação VARCHAR(40),
-    CONSTRAINT fk_empresa FOREIGN KEY (fk_Empresa) REFERENCES CADASTRO(UserID),
-    CONSTRAINT chk_instalacao CHECK (Instalacao IN('Geladeira','Caminhão','Câmara Fria'))
+    CONSTRAINT fk_empresa FOREIGN KEY (fk_Empresa) REFERENCES EMPRESA(IDEmpresa),
+    CONSTRAINT chk_instalacao CHECK (Instalacao IN('fábrica', 'caminhão', 'câmara fria'))
 );
-CREATE TABLE TEMP_UMID (
-    ID INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE REGISTRO (
+    IDRegistro INT PRIMARY KEY AUTO_INCREMENT,
     DtHora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     fk_Sensor INT,
     Temperatura INT,
     Umidade INT,
     CONSTRAINT fk_sensor FOREIGN KEY (fk_Sensor) REFERENCES SENSOR(SensorID)
 );
-
