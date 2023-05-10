27f57a673b347f88c22242250b692309be2c8f4c
NicolasNun <nicolas.nsantos@sptech.school>
2023-05-10 07:52:35 -0300

    Remoção dos inserts no banco

--- Banco de Dados/Banco de dados V3.sql
+++ Banco de Dados/Banco de dados V3.sql
@@ -8,11 +8,6 @@ nomeFantasia varchar(45),
 cnpj char(18)
 ) auto_increment = 100;
 
-insert into empresa values
-(null, 'Friboi', '12.345.678/9012-34'),
-(null, 'Swift', '11.455.632/2212-12'),
-(null, 'Minerva Foods', '17.777.213/9999-32');
-
 select * from empresa;
 
 create table usuario(
@@ -23,11 +18,6 @@ fkEmpresa int,
 constraint fkEmpresa foreign key (fkEmpresa) references empresa (idEmpresa),
 constraint pkCompostaUsu primary key (idUsuario,fkEmpresa)
 ) auto_increment = 200;
-
-insert into usuario values
-(null, 'jorge.aragao@sptech.school', '12345678', 100),
-(null, 'paulocoelho@gmail.com', '28193048', 101),
-(null, 'paulo.plinio@sptech.school', '58627493', 102);
 
 select * from usuario;
 
@@ -41,11 +31,6 @@ constraint fkEmpresaLocal foreign key (fkEmpresaLocal) references empresa (idEmp
 constraint pkCompostaLocal primary key (idLocal,fkEmpresaLocal)
 ) auto_increment = 300;
 
-insert into localizacao values
-(null, '12345-678', 'Geladeira do 1° andar', '45', 100),
-(null, '12341-700', 'Caminhão 7', '46', 101),
-(null, '14087-843', 'Fábrica SP', '47', 102);
-
 select * from localizacao;
 
 create table sensor(
@@ -58,11 +43,6 @@ constraint fkLocal foreign key (fkLocal) references localizacao (idLocal),
 constraint chkSituacao check (situacao in (0, 1))
 ) auto_increment = 400;
 
-insert into sensor values
-(null, '1', 'caminhão', 300),
-(null, '1', 'câmara', 301),
-(null, '1', 'geladeira', 302);
-
 select * from sensor;
 
 create table registro(
@@ -74,11 +54,6 @@ fkSensor int,
 constraint fkSensor foreign key (fkSensor) references Sensor(idSensor),
 constraint pkCompostaRegi primary key (idRegistro,fkSensor)
 ) auto_increment = 500;
-
-insert into registro (temperatura, umidade, fkSensor) values
-('2', '50', 400),
-('0', '55', 401),
-('4', '52', 402);
 
 select * from registro;
 
