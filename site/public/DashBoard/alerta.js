ed3a738e9d6667d6154910b03d17d50a15cb2968
Rian Ferrari <rianferrari37@gmail.com>
2023-05-31 01:14:13 -0300

    Configurando Toast

--- site/public/DashBoard/alerta.js
+++ site/public/DashBoard/alerta.js
@@ -1,5 +1,38 @@
 var alertas = [];
 
+function exibirToast(msg) {
+    let toastBox = document.getElementById('alertasBox');
+
+    // Toast de Temperatura
+
+    // let alertaAmareloTemp = 'Alerta Amarelo - Sensor 2 Ultrapassou de 2,95°C'; 
+    // let alertaAzulTemp = 'Alerta Azul - Sensor 3 Está abaixo de 0,07°C'; 
+    // let alertaRoxoTemp = 'Alerta Roxo - Sensor 4 Está abaixo de 0°C'; 
+
+    // // Toast de Umidade
+
+    // let alertaVermelhoUmi = 'Alerta Vermelho - Sensor 1 Ultrapassou de UR 95%'; 
+    // let alertaAmareloUmi = 'Alerta Amarelo - Sensor 2 Ultrapassou de UR 92,85%'; 
+    // let alertaAzulUmi = 'Alerta Azul - Sensor 3 Está abaixo de UR 84%'; 
+    // let alertaRoxoUmi = 'Alerta Roxo - Sensor 4 Está abaixo de UR 80%';
+
+    obterdados(5000)
+    botaoTeste(toastBox, msg)
+
+    
+}
+
+function botaoTeste(toastBox, msg) {
+    let toast = document.createElement('div');
+    toast.classList.add('toast');
+    toast.innerHTML = msg;
+    toastBox.appendChild(toast)
+
+    setTimeout(() => {
+        toast.remove()
+    }, 6000);
+}
+
 function obterdados(idSensor) {
     fetch(`/medidas/tempo-real/${idSensor}`)
         .then(resposta => {
@@ -22,51 +55,58 @@ function obterdados(idSensor) {
 }
 
 function alertar(resposta, idSensor) {
-    var temp = resposta[0].temperatura;
-
-    console.log(idSensor === resposta[0].fkSensor)
     
-    var grauDeAviso ='';
+    var temp = resposta[0].temperatura;
+    let msg = ""
+
+    if(temp >= 4) {
+        msg = "Muito quente"
+        exibirToast(msg)
+    }
+
+    // console.log(idSensor === resposta[0].fkSensor)
+    
+    // var grauDeAviso ='';
 
 
-    var limites = {
-        muito_quente: 4,
-        quente: 3.78,
-        ideal: 2.34,
-        frio: 1.80,
-        muito_frio: 0
-    };
+    // var limites = {
+    //     muito_quente: 4,
+    //     quente: 3.78,
+    //     ideal: 2.34,
+    //     frio: 1.80,
+    //     muito_frio: 0
+    // };
 
-    var classe_temperatura = 'cor-alerta';
+    // var classe_temperatura = 'cor-alerta';
 
-    if (temp >= limites.muito_quente) {
-        classe_temperatura = 'cor-alerta perigo-quente';
-        grauDeAviso = 'perigo quente'
-        grauDeAvisoCor = 'cor-alerta perigo-quente'
-        exibirAlerta(temp, idSensor, grauDeAviso, grauDeAvisoCor)
-    }
-    else if (temp < limites.muito_quente && temp >= limites.quente) {
-        classe_temperatura = 'cor-alerta alerta-quente';
-        grauDeAviso = 'alerta quente'
-        grauDeAvisoCor = 'cor-alerta alerta-quente'
-        exibirAlerta(temp, idSensor, grauDeAviso, grauDeAvisoCor)
-    }
-    else if (temp < limites.quente && temp > limites.frio) {
-        classe_temperatura = 'cor-alerta ideal';
-        removerAlerta(idSensor);
-    }
-    else if (temp <= limites.frio && temp > limites.muito_frio) {
-        classe_temperatura = 'cor-alerta alerta-frio';
-        grauDeAviso = 'alerta frio'
-        grauDeAvisoCor = 'cor-alerta alerta-frio'
-        exibirAlerta(temp, idSensor, grauDeAviso, grauDeAvisoCor)
-    }
-    else if (temp <= limites.muito_frio) {
-        classe_temperatura = 'cor-alerta perigo-frio';
-        grauDeAviso = 'perigo frio'
-        grauDeAvisoCor = 'cor-alerta perigo-frio'
-        exibirAlerta(temp, idSensor, grauDeAviso, grauDeAvisoCor)
-    }
+    // if (temp >= limites.muito_quente) {
+    //     classe_temperatura = 'cor-alerta perigo-quente';
+    //     grauDeAviso = 'perigo quente'
+    //     grauDeAvisoCor = 'cor-alerta perigo-quente'
+    //     exibirAlerta(temp, idSensor, grauDeAviso, grauDeAvisoCor)
+    // }
+    // else if (temp < limites.muito_quente && temp >= limites.quente) {
+    //     classe_temperatura = 'cor-alerta alerta-quente';
+    //     grauDeAviso = 'alerta quente'
+    //     grauDeAvisoCor = 'cor-alerta alerta-quente'
+    //     exibirAlerta(temp, idSensor, grauDeAviso, grauDeAvisoCor)
+    // }
+    // else if (temp < limites.quente && temp > limites.frio) {
+    //     classe_temperatura = 'cor-alerta ideal';
+    //     removerAlerta(idSensor);
+    // }
+    // else if (temp <= limites.frio && temp > limites.muito_frio) {
+    //     classe_temperatura = 'cor-alerta alerta-frio';
+    //     grauDeAviso = 'alerta frio'
+    //     grauDeAvisoCor = 'cor-alerta alerta-frio'
+    //     exibirAlerta(temp, idSensor, grauDeAviso, grauDeAvisoCor)
+    // }
+    // else if (temp <= limites.muito_frio) {
+    //     classe_temperatura = 'cor-alerta perigo-frio';
+    //     grauDeAviso = 'perigo frio'
+    //     grauDeAvisoCor = 'cor-alerta perigo-frio'
+    //     exibirAlerta(temp, idSensor, grauDeAviso, grauDeAvisoCor)
+    // }
 
 /*     var card;
 
@@ -108,20 +148,20 @@ function removerAlerta(idSensor) {
 }
  
 function exibirCards() {
-    alerta.innerHTML = '';
+    alertasBox.innerHTML = '';
 
     for (var i = 0; i < alertas.length; i++) {
         var mensagem = alertas[i];
-        alerta.innerHTML += transformarEmDiv(mensagem);
+        alertasBox.innerHTML += transformarEmDiv(mensagem);
     }
 }
 
-function transformarEmDiv({ idSensor, temp, grauDeAviso, grauDeAvisoCor }) {
-    return `<div class="mensagem-alarme">
-    <div class="informacao">
-    <div class="${grauDeAvisoCor}">&#12644;</div> 
-     <h3>Aquário ${idSensor} está em estado de ${grauDeAviso}!</h3>
-    <small>Temperatura ${temp}.</small>   
-    </div>
-    </div>`;
-}
+// function transformarEmDiv({ idSensor, temp, grauDeAviso, grauDeAvisoCor }) {
+//     return `<div class="alertasToatsBox">
+//     <div class="toast">
+//     <div class="${grauDeAvisoCor}">&#12644;</div> 
+//      <h3>Aquário ${idSensor} está em estado de ${grauDeAviso}!</h3>
+//     <small>Temperatura ${temp}.</small>   
+//     </div>
+//     </div>`;
+// }
